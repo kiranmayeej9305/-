83ce0bd3c40c13769b8b1627ca80bbc85827c5ee
Manas Nayak <kiranmayeej9305@gmail.com>
2024-10-21 17:57:07 -0500

    qweqw

--- next.config.js
+++ next.config.js
@@ -13,22 +13,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
-  experimental: {
-    incrementalBuild: true,
-    optimizeCss: true,
-    optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
